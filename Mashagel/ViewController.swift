ba8f008b63d4d13d81b0b7c4f10e8ff3e458a12f
AbdelRahman Aref <abdelrahman_aref@hotmail.com>
2019-03-03 11:26:38 +0200

    Initial Commit

---
+++ Mashagel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mashagel
+//
+//  Created by AbdelRahman Aref on 3/3/19.
+//  Copyright © 2019 AbdelRahman Aref. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
